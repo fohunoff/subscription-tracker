1529d405c18ce9868dc70021fa894c9fc3d690f0
Ivan Zagainov <fohunoff@gmail.com>
2026-01-05 15:02:18 +0100

    fix: add hash to build files

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // Отключаем кеширование в dev режиме
+    headers: {
+      'Cache-Control': 'no-store',
+    },
+  },
+  build: {
+    // Добавляем хеш к файлам при сборке
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
+      }
+    }
+  }
 })
